493e847d281674849daf5306773ec52ea66fb2b8
Mimas <michal.maselko@awesome-ind.com>
2022-08-10 20:58:08 +0200

    .

--- my-app/pages/_app.tsx
+++ my-app/pages/_app.tsx
@@ -3,7 +3,14 @@ import type { AppProps } from 'next/app'
 import Layout from '../components/Layout/Layout'
 import { StoreProvider } from '../store/StoreProvider'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, ...appProps }: AppProps) {
+  if([`/form-details`].includes(appProps.router.pathname)){
+    return(
+      <StoreProvider>
+        <Component {...pageProps} />
+      </StoreProvider>
+    )
+  }
   return(
     <Layout>
       <StoreProvider>
